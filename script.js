b703d42db6eaf0535c9f5b0aea0aea57a4d36162
a41ya <180646616+a41ya@users.noreply.github.com>
2026-08-19 17:50:26 +0300

    Add play round logic

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ function getComputerChoice () {
  }
 }
 
-console.log (getComputerChoice());
-
 
 //human choice function
 function getHumanChoice () {
@@ -28,8 +26,44 @@ function getHumanChoice () {
   }
 }
 
-console.log(getHumanChoice());
 
 //score variables
 let humanScore = 0;
 let computerScore = 0;
+
+
+//play round logic
+function playRound (humanChoice, computerChoice) {
+  const lose = "lose.";
+  const tie = "tie";
+  const win = "win!";
+
+  if (humanChoice === "rock" && computerChoice === "rock") {
+    return tie;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    return win;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    return lose;
+  }
+
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+    return lose;
+  } else if (humanChoice === "paper" && computerChoice === "paper") {
+    return tie;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    return win;
+  }
+
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+      return win;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    return lose;
+  } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+    return tie;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log (playRound(humanSelection, computerSelection));
