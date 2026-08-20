23729ceccf2836b73c55c666da47cf290114ed17
a41ya <180646616+a41ya@users.noreply.github.com>
2026-08-20 13:32:50 +0300

    Add play game logic

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function getHumanChoice () {
   } else if (choice === "scissors"){
     return "scissors";
   } else {
-    return "you don't choose anything";
+    return "nothing";
   }
 }
 
@@ -34,36 +34,54 @@ let computerScore = 0;
 
 //play round logic
 function playRound (humanChoice, computerChoice) {
-  const lose = "lose.";
-  const tie = "tie";
-  const win = "win!";
 
   if (humanChoice === "rock" && computerChoice === "rock") {
-    return tie;
+    return "tie";
   } else if (humanChoice === "paper" && computerChoice === "rock") {
-    return win;
+    return "win";
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    return lose;
+    return "lose";
   }
 
-    else if (humanChoice === "rock" && computerChoice === "paper") {
-    return lose;
+  else if (humanChoice === "rock" && computerChoice === "paper") {
+    return "lose";
   } else if (humanChoice === "paper" && computerChoice === "paper") {
-    return tie;
+    return "tie";
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    return win;
+    return "win";
   }
 
-    else if (humanChoice === "rock" && computerChoice === "scissors") {
-      return win;
+  else if (humanChoice === "rock" && computerChoice === "scissors") {
+    return "win";
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    return lose;
+    return "lose";
   } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-    return tie;
+    return "tie";
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-console.log (playRound(humanSelection, computerSelection));
+//round result with log in console
+function playGame () {
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+
+  const result = playRound(humanSelection, computerSelection);
+
+  if (result === "win") {
+    humanScore++;
+  } else if (result === "lose") {
+    computerScore++;
+  }
+  
+  console.log (`you choose ${humanSelection}, computer choose ${computerSelection}. result is a ${result}`)
+  console.log (humanScore, computerScore);
+
+}
+
+//temporary until i learn loops
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
